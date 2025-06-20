dbb4144340876233c6366639714d49bce228affa
Marc C. <marc@setfive.com>
2025-06-20 12:21:04 -0400

    setfive/system: update deprecations

--- systems/setfive/system.nix
+++ systems/setfive/system.nix
@@ -82,8 +82,8 @@
 
     ### Gnome
     services.xserver.enable = true;
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
     environment.gnome.excludePackages = with pkgs; [
         orca
         evince
